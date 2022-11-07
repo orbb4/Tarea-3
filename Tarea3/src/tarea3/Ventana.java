215f3b79be2afc433f33bcaea6acad66d68d70a7
cotecita <marcastillo2021@udec.cl>
2022-11-07 17:49:20 -0300

    se alinean las monedas y se realizan cambios en su aspecto

--- Tarea3/src/tarea3/Ventana.java
+++ Tarea3/src/tarea3/Ventana.java
@@ -33,30 +33,17 @@ public class Ventana extends JFrame {
     private void Botones(){
         JButton boton1 = new JButton(); //en el constructor/argumento le podemos pasar texto entre comillas
         //this.setLayout(null); testing
-        boton1.setText("CocaCola"); //se le establece un texto al botón
-        boton1.setBounds(620, 180, 85, 85);
-        boton1.setFont(new Font("cooper black",3,12));
+        boton1.setBounds(655, 180, 50, 50);
         boton1.setEnabled(true);// si está en false no se puede hacer click sobre el botón, es para habilitar o deshabilitar las funciones del botón
-        //boton1.setForeground(Color.GREEN);//se le asigna un color a la letra del botón
         this.add(boton1);
         
         JButton boton2 = new JButton(); //en el constructor/argumento le podemos pasar texto entre comillas
-        boton2.setText("Sprite"); //se le establece un texto al botón
-        boton2.setBounds(620, 360, 70, 70);
-        boton2.setFont(new Font("cooper black",3,12));
+        boton2.setBounds(655, 280, 50, 50);
         boton2.setEnabled(true);// si está en false no se puede hacer click sobre el botón, es para habilitar o deshabilitar las funciones del botón
-        //boton2.setForeground(Color.GREEN);//se le asigna un color a la letra del botón
         this.add(boton2);
         
         JButton boton3 = new JButton(); //en el constructor/argumento le podemos pasar texto entre comillas
-        boton3.setText("Fanta"); //se le establece un texto al botón
-<<<<<<< HEAD
-        boton3.setBounds(620, 540, 70, 70);
-        boton3.setFont(new Font("baguet script",3,12));
-=======
-        boton3.setBounds(100, 300, 100, 40);
-        //boton3.setBorder(new RoundedBorder(10));
->>>>>>> beta
+        boton3.setBounds(655, 380, 50, 50);
         boton3.setEnabled(true);// si está en false no se puede hacer click sobre el botón, es para habilitar o deshabilitar las funciones del botón
         //boton3.setForeground(Color.GREEN);//se le asigna un color a la letra del botón
         this.add(boton3);
