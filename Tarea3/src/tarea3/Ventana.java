91153ea66d5b1888c12c4221a8d3c58a327f27b7
orbb4 <reiturra2021@udec.cl>
2022-11-07 14:56:34 -0300

    correccion de errores menores

--- Tarea3/src/tarea3/Ventana.java
+++ Tarea3/src/tarea3/Ventana.java
@@ -1,26 +1,28 @@
 package tarea3;
 import java.awt.*;
 import javax.swing.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-
-    
+import javax.swing.ImageIcon;
 
 public class Ventana extends JFrame {
     Moneda moneda = new Moneda();
+
     Expendedor expendedor = new Expendedor(3, 100);
     Comprador comprador = new Comprador(moneda, 2, expendedor);
-    private final Color COLOR_PANEL = new Color(50, 50, 160);
+    
+    final Color COLOR_PANEL = new Color(50, 50, 160);
+    //Paneles expPanel = new Paneles(COLOR_PANEL, new int[] {120, 100, 600, 900}, comprador);
+    
     public Ventana(){
-    super();
-    this.setLayout(new BorderLayout());
-    this.setTitle("Expendedor");
-    this.setDefaultCloseOperation(EXIT_ON_CLOSE); 
-    this.setSize(1000,800); 
-    this.setVisible(true); 
-    this.setResizable(false);
-    this.add(new Paneles(COLOR_PANEL, new int[] {120, 100, 600, 900}, comprador));
+        super();
+        //expPanel.add(new JButton("sskdkdsf"), BorderLayout.EAST);
+        this.setLayout(new BorderLayout());
+        this.setTitle("Expendedor");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE); 
+        this.setSize(1000,800); 
+        this.setVisible(true); 
+        this.setResizable(false);
+
+        this.add(new Paneles(COLOR_PANEL, new int[] {120, 100, 600, 900}, comprador));
    } 
 }
 
