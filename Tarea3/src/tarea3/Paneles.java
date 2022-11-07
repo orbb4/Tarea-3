5ab74f83ba06e2baa711fd3a349e9c44bc7b7202
orbb4 <reiturra2021@udec.cl>
2022-11-07 13:51:52 -0300

    añadido metodo paint en paneles

--- Tarea3/src/tarea3/Paneles.java
+++ Tarea3/src/tarea3/Paneles.java
@@ -1,13 +1,18 @@
 package tarea3;
-
-import java.awt.Color;
-import javax.swing.JPanel;
-
+import java.awt.*;
+import javax.swing.*;
 public class Paneles extends JPanel{
         private Color color;
-        public Paneles(Color color){
+        private int[] rect; // dimensiones del rectangulo (x, y, ancho, alto)
+        public Paneles(Color color, int[] rect){
             this.setBackground(this.color); //se le asigna un color al panel
             this.setLayout(null);
+            this.rect = rect;
         }
+        public void paint(Graphics g){
+            super.paint(g);
+            g.setColor(color);
+            g.fillRect(rect[0], rect[1], rect[2], rect[3]);
+    }
 
 }
--- Tarea3/src/tarea3/Ventana.java
+++ Tarea3/src/tarea3/Ventana.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Ventana extends JFrame { 
-
+    private final Color COLOR_PANEL = new Color(50, 50, 160);
     public Ventana(){
     super();
     this.setLayout(new BorderLayout());
@@ -12,6 +12,7 @@ public class Ventana extends JFrame {
     this.setSize(1000,800); 
     this.setVisible(true); 
     this.setResizable(false);
+    this.add(new Paneles(COLOR_PANEL, new int[] {0, 0, 250, 250}));
    } 
 }
 
