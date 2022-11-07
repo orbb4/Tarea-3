0370c497fb0727d2a93544ac4ed3a63ea4156fe6
cotecita <marcastillo2021@udec.cl>
2022-11-07 15:38:19 -0300

    se intenta crear botones

--- Tarea3/src/tarea3/Paneles.java
+++ Tarea3/src/tarea3/Paneles.java
@@ -3,15 +3,10 @@ import java.awt.*;
 import javax.swing.*;
 public class Paneles extends JPanel{
         private Color color;
-<<<<<<< HEAD
         private int[] rect; // dimensiones del rectangulo (x, y, ancho, alto)
-        public Paneles(Color color, int[] rect){
-=======
         private Comprador comprador;
-        private int[] rect; // dimensiones del rectangulo (x, y, ancho, alto)
         public Paneles(Color color, int[] rect, Comprador com){
             comprador = com;
->>>>>>> beta
             this.setBackground(this.color); //se le asigna un color al panel
             this.setLayout(null);
             this.rect = rect;
@@ -20,10 +15,9 @@ public class Paneles extends JPanel{
             super.paint(g);
             g.setColor(color);
             g.fillRect(rect[0], rect[1], rect[2], rect[3]);
-<<<<<<< HEAD
-=======
+
             comprador.paint(g);
->>>>>>> beta
+
     }
         
 }
--- Tarea3/src/tarea3/Ventana.java
+++ Tarea3/src/tarea3/Ventana.java
@@ -3,23 +3,6 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.ImageIcon;
 
-/*public class Ventana extends JFrame { 
-    private final Color COLOR_PANEL = new Color(50, 50, 160);
-    private final Color COLOR_DEPOSITO_COCACOLA = new Color(0, 128, 0);
-    private Paneles panelCocacola = new Paneles(COLOR_DEPOSITO_COCACOLA, new int[] {500,300, 200, 100});
-    public Ventana(){
-    super();
-    this.setLayout(new BorderLayout());
-    this.setTitle("Expendedor");
-    this.setDefaultCloseOperation(EXIT_ON_CLOSE); 
-    this.setSize(1000,800); 
-    this.setVisible(true); 
-    this.setResizable(false);
-    this.add(new Paneles(COLOR_PANEL, new int[] {0, 0, 250, 250}));
-    this.add(panelCocacola);
-    this.add(new Boton(":D", 250, 250, 40, 40));
-    panelCocacola.add(new JButton("este"), BorderLayout.EAST);
-=======*/
 public class Ventana extends JFrame {
     Moneda moneda = new Moneda();
 
@@ -39,12 +22,9 @@ public class Ventana extends JFrame {
         this.setVisible(true); 
         this.setResizable(false);
 
-<<<<<<< HEAD
-        this.add(new Paneles(COLOR_PANEL, new int[] {120, 100, 600, 900}, comprador));t
-=======
+
         this.add(new Paneles(COLOR_PANEL, new int[] {120, 100, 600, 900}, comprador));
         this.add(comprador);
->>>>>>> beta
    } 
 }
 
