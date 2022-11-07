ea26ecfbd4952ca4de2f7280fe5104dab13446f9
cotecita <marcastillo2021@udec.cl>
2022-11-07 13:27:42 -0300

    se agrega clase panel

--- Tarea3/src/tarea3/Tarea3.java
+++ Tarea3/src/tarea3/Tarea3.java
@@ -1,13 +1,9 @@
 package tarea3;
 public class Tarea3 {
     public static void main(String[] args) {
-<<<<<<< HEAD
         Ventana v = new Ventana(); //abre la ventana
         v.setVisible(true);//se hace visible la ventana, por defecto viene en false
-=======
-        Ventana v1 = new Ventana(); //abre la ventana
-        v1.setVisible(true);//se hace visible la ventana, por defecto viene en false
->>>>>>> 31ba7c364df41c7ef81011e57ec421ae45b2ebf6
+        
     }
     
 }
--- Tarea3/src/tarea3/Ventana.java
+++ Tarea3/src/tarea3/Ventana.java
@@ -1,19 +1,11 @@
 package tarea3;
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 3d2655a8b997aef8babd4d064dab8a0130ec9f36
 import java.awt.*;
 import javax.swing.*;
 
-<<<<<<< HEAD
 public class Ventana extends JFrame { // como extiende a un JFrame, es un JFrame, pero con algunos agregados
      // propiedades que puedes agregar aquí para que compongan a Ventana. Ventana "es un" JFrame
-    public JPanel panel;
-=======
-public class Ventana extends JFrame {
->>>>>>> 31ba7c364df41c7ef81011e57ec421ae45b2ebf6
+
     public Ventana(){
     super();
     this.setLayout(new BorderLayout());
