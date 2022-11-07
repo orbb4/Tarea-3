5e3a4c8ca04c8f1271418667235f0ecf99f582aa
orbb4 <reiturra2021@udec.cl>
2022-11-07 17:39:02 -0300

    añadidas imagenes de todas las bebidas al expendedor

--- Tarea3/src/tarea3/Ventana.java
+++ Tarea3/src/tarea3/Ventana.java
@@ -31,7 +31,6 @@ public class Ventana extends JFrame {
     private void Botones(){
         JButton boton1 = new JButton(); //en el constructor/argumento le podemos pasar texto entre comillas
         //this.setLayout(null); testing
-        boton1.setText("Click"); //se le establece un texto al botón
         boton1.setText("CocaCola"); //se le establece un texto al botón
         boton1.setBounds(100, 100, 100, 40);
         boton1.setEnabled(true);// si está en false no se puede hacer click sobre el botón, es para habilitar o deshabilitar las funciones del botón
@@ -48,9 +47,12 @@ public class Ventana extends JFrame {
         JButton boton3 = new JButton(); //en el constructor/argumento le podemos pasar texto entre comillas
         boton3.setText("Fanta"); //se le establece un texto al botón
         boton3.setBounds(100, 300, 100, 40);
+        //boton3.setBorder(new RoundedBorder(10));
         boton3.setEnabled(true);// si está en false no se puede hacer click sobre el botón, es para habilitar o deshabilitar las funciones del botón
         //boton3.setForeground(Color.GREEN);//se le asigna un color a la letra del botón
         this.add(boton3);
+        
+        
     }
 }
 
