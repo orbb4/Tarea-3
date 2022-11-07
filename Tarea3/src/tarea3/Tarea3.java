3d2655a8b997aef8babd4d064dab8a0130ec9f36
cotecita <marcastillo2021@udec.cl>
2022-11-07 13:11:19 -0300

    se corrigen errores

--- Tarea3/src/tarea3/Tarea3.java
+++ Tarea3/src/tarea3/Tarea3.java
@@ -1,12 +1,8 @@
 package tarea3;
 public class Tarea3 {
     public static void main(String[] args) {
-<<<<<<< HEAD
         Ventana v = new Ventana(); //abre la ventana
-=======
-        Ventana v1 = new Ventana(); //se crea la ventana
         v1.setVisible(true);//se hace visible la ventana, por defecto viene en false
->>>>>>> cote
     }
     
 }
