fa5f25870f66d730d492a3677139b95120bf0b46
cotecita <marcastillo2021@udec.cl>
2022-11-08 10:45:38 -0300

    se modifica clase monedas

--- Tarea3/src/tarea3/Moneda.java
+++ Tarea3/src/tarea3/Moneda.java
@@ -1,10 +1,10 @@
 package tarea3;
 
 class Moneda{
-    private String serie;
+    /*private String serie;
     Moneda(String serie){
         this.serie=serie;
-    }
+    }*/
 
     public String getSerie(){
         return this.toString();
@@ -14,24 +14,24 @@ class Moneda{
     }
 }
 class Moneda100 extends Moneda{
-    Moneda100(String n1){
-        super(n1);
+    Moneda100(){
+       // super(n1);
     }
     public int getValor(){
         return 100;
     }
 }
 class Moneda500 extends Moneda{
-    Moneda500(String n2){
-        super(n2);
+    Moneda500(){
+        //super(n2);
     }
     public int getValor(){
         return 500;
     }
 }
 class Moneda1000 extends Moneda{
-    Moneda1000(String n3){
-        super(n3);
+    Moneda1000( ){
+        //super(n3);
     }
     public int getValor(){
         return 1000;
